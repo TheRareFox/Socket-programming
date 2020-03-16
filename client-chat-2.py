51ee3be561fbff49e18a637c479f320548d92e12
Zhang Yuxiang <47800388+UwUUwU@users.noreply.github.com>
2020-03-16 22:01:35 +0800

    added comments

--- client-chat-2.py
+++ client-chat-2.py
@@ -1,19 +1,29 @@
 import socket
-chat_socket = socket.socket()
-chat_socket.connect(('127.0.0.1',6789))
-
+#Creates a new socket
+my_socket = socket.socket()
+#Gets the address of the server
+address = input('Enter IPv4 address of server: ')
+#Gets the port of the server
+port = int(input('Enter port number of server: '))
+#Connects to the server
+my_socket.connect((address, port))
 quit = False
+#when users havent quitted yet
 while not quit:
     print("WAITING FROM SERVER")
     data = ''
     while '\n' not in data:
         data += chat_socket.recv(1024).decode()
+    #collects data from server until server ends the line
     print("SERVER WROTE: {}".format(data))
+    #if server entered quit then stop running the code
     if data == 'quit\n':
         quit = True
     else:
         data = input("INPUT CLIENT: ")
+        #if user entered data stop running the code
         if data == 'quit':
             quit = True
+        #sends data over to the server
         chat_socket.sendall(data.encode() + b'\n')
 chat_socket.close()
