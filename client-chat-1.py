f340b27e03c406e870f6ef0fe83f56f0dba4e14f
Zhang Yuxiang <47800388+UwUUwU@users.noreply.github.com>
2020-03-16 21:59:35 +0800

    Update client-chat-1.py

--- client-chat-1.py
+++ client-chat-1.py
@@ -1,13 +1,20 @@
 import socket
-chat_socket = socket.socket()
+#Creates a new socket
+my_socket = socket.socket()
+#Gets the address of the server
 address = input('Enter IPv4 address of server: ')
+#Gets the port of the server
 port = int(input('Enter port number of server: '))
-chat_socket.connect((address, port))
+#Connects to the server
+my_socket.connect((address, port))
+#continuously run the code
 while True:
     print('WAITING FOR SERVER...')
+    #collects data from server until server ends the line
     data = b''
     while b'\n' not in data:
         data += chat_socket.recv(1024)
-    print('SERVER WROTE: ' + data.decode())
+    print('SERVER WROTE: ' + data.decode()
     data = input('INPUT CLIENT: ').encode()
+    #sends data to the server
     chat_socket.sendall(data + b'\n')
