798152e4262b35b015f71cda0935f46f4f2683c6
Zhang Yuxiang <47800388+UwUUwU@users.noreply.github.com>
2020-03-16 21:58:05 +0800

    added comments

--- client-2.py
+++ client-2.py
@@ -1,9 +1,14 @@
 import socket
+#Creates a new socket
 my_socket = socket.socket()
+#Gets the address of the server
 address = input('Enter IPv4 address of server: ')
+#Gets the port of the server
 port = int(input('Enter port number of server: '))
+#Connects to the server
 my_socket.connect((address, port))
 data = b''
+#collects data from server until server ends the liine
 while b'\n' not in data:
     data += my_socket.recv(1024)
 print(data)
