1fb39284e7782ab706496cfd63f5e7b823f4f632
Zhang Yuxiang <47800388+UwUUwU@users.noreply.github.com>
2020-03-16 21:53:27 +0800

    added comments

--- client-1.py
+++ client-1.py
@@ -1,10 +1,15 @@
 import socket
+#Creates a new socket
 my_socket = socket.socket()
+#Gets the address of the server
 address = input('Enter IPv4 address of server: ')
+#Gets the port of the server
 port = int(input('Enter port number of server: '))
+#all the names of the host
 host = socket.gethostname()
+#Connects to the server
 my_socket.connect((address,port))
-
+#prints out the data recieved from server
 print(my_socket.recv(1024))
 my_socket.close()
 
