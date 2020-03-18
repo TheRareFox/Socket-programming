cbb69494e30b618ad57783bd3dc2b08a6c61da8b
Zhang Yuxiang <47800388+UwUUwU@users.noreply.github.com>
2020-03-18 19:51:33 +0800

    Update server-1.py

--- server-1.py
+++ server-1.py
@@ -1,10 +1,15 @@
 import socket
+#Creates a new socket
 my_socket = socket.socket()
+#Creates a server at address local host and port number 12345
 my_socket.bind(("127.0.0.1", 12345))
+#listens for active connections from the client
 my_socket.listen()
-
+#accepts connections from the client and returns the socket to send data to client
 new_socket, addr = my_socket.accept()
 print('Connected to: ' + str(addr))
+#sends bytes over to the client
 new_socket.sendall(b'Hello from server\n')
+#closes the connection
 new_socket.close()
 my_socket.close()
